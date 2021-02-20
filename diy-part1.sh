61c9c0039f504f302a69f9dd85b9ec52042a8fda
wlsk888 <55219362+wlsk888@users.noreply.github.com>
2021-02-20 12:21:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,22 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-vssr package/diy/luci-app-vssr
+#svn co https://github.com/garypang13/openwrt-packages/trunk/shadowsocksr-libev package/diy/shadowsocksr-libev
+#rm -rf feeds/kenzo/luci-app-vssr
+#rm -rf feeds/small/shadowsocksr-libev
+#rm -rf package/feeds/kenzo/luci-app-vssr
+#rm -rf package/feeds/small/shadowsocksr-libev
+#自动挂载
+#svn co https://github.com/garypang13/openwrt-packages/trunk/automount package/diy/automount
+#svn co https://github.com/garypang13/openwrt-packages/trunk/autosamba package/diy/autosamba
+#主题
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/diy/luci-theme-argon
+#svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-argon-config package/diy/luci-app-argon-config
+svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-zerotier package/diy/luci-app-zerotier
+svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-accesscontrol package/diy/luci-app-accesscontrol
+svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-autoreboot package/diy/luci-app-autoreboot
+svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-socat package/diy/luci-app-socat
+svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-ramfree package/diy/luci-app-ramfree
+git clone https://github.com/small-5/ddns-scripts-dnspod package/diy/ddns-scripts-dnspod
+svn co https://github.com/garypang13/openwrt-packages/trunk/ddns-scripts-aliyun package/diy/ddns-scripts-aliyun
