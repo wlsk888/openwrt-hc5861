1f2f45930022e945b7220f41bc9af0918dce9fa4
wlsk888 <55219362+wlsk888@users.noreply.github.com>
2021-02-21 20:50:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,22 +16,22 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 #zerotier汉化
-sed -i 's/msgid "Interface Info"/msgid "Base Setting"/g' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i 's/msgstr "接口信息"/msgstr "基本设置"/g' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i '$a \\' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i '$a msgid "Interface Info"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i '$a msgstr "接口信息"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i '$a \\' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i '$a msgid "NOT RUNNING"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i '$a msgstr "未运行"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i '$a \\' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i '$a msgid "RUNNING"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i '$a msgstr "运行中"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+#sed -i 's/msgid "Interface Info"/msgid "Base Setting"/g' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+#sed -i 's/msgstr "接口信息"/msgstr "基本设置"/g' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+#sed -i '$a \\' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+#sed -i '$a msgid "Interface Info"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+#sed -i '$a msgstr "接口信息"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+#sed -i '$a \\' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+#sed -i '$a msgid "NOT RUNNING"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+#sed -i '$a msgstr "未运行"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+#sed -i '$a \\' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+#sed -i '$a msgid "RUNNING"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+#sed -i '$a msgstr "运行中"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
 #Socat汉化
-sed -i 's/msgstr "Socat"/msgstr "端口转发"/g' package/diy/luci-app-socat/po/zh_Hans/socat.po
-sed -i '$a \\' package/diy/luci-app-socat/po/zh_Hans/socat.po
-sed -i '$a msgid "APPLY"' package/diy/luci-app-socat/po/zh_Hans/socat.po
-sed -i '$a msgstr "启用"' package/diy/luci-app-socat/po/zh_Hans/socat.po
-sed -i '$a \\' package/diy/luci-app-socat/po/zh_Hans/socat.po
-sed -i '$a msgid "Apply"' package/diy/luci-app-socat/po/zh_Hans/socat.po
-sed -i '$a msgstr "启用"' package/diy/luci-app-socat/po/zh_Hans/socat.po
+sed -i 's/msgstr "Socat"/msgstr "端口转发"/g' package/lean/luci-app-socat/po/zh_Hans/socat.po
+sed -i '$a \\' package/lean/luci-app-socat/po/zh_Hans/socat.po
+sed -i '$a msgid "APPLY"' package/lean/luci-app-socat/po/zh_Hans/socat.po
+sed -i '$a msgstr "启用"' package/lean/luci-app-socat/po/zh_Hans/socat.po
+sed -i '$a \\' package/lean/luci-app-socat/po/zh_Hans/socat.po
+sed -i '$a msgid "Apply"' package/lean/luci-app-socat/po/zh_Hans/socat.po
+sed -i '$a msgstr "启用"' package/lean/luci-app-socat/po/zh_Hans/socat.po
